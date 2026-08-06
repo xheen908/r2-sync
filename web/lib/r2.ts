06c29ceef2b53f001aacf735c24c854ffc51a526
Arndt Christoph Handschuh <xheen908@macbooklan.fritz.box>
2026-08-07 01:58:47 +0200

    chore: Update Cloudflare R2 S3 access keys in web app configuration

--- web/lib/r2.ts
+++ web/lib/r2.ts
@@ -1,8 +1,8 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 const accountId = process.env.R2_ACCOUNT_ID || "10c9109e9e342e2b4fc55e71ddf91c17";
-const accessKeyId = process.env.R2_ACCESS_KEY_ID || "6e87984a4bbe49caaee83a4d3eee39a0";
-const secretAccessKey = process.env.R2_SECRET_ACCESS_KEY || "";
+const accessKeyId = process.env.R2_ACCESS_KEY_ID || "763791a177749b6538807006271e358f";
+const secretAccessKey = process.env.R2_SECRET_ACCESS_KEY || "bddfd02961a71996e0e6e078a0360a28082c52799fcc72e2a91615db58469b2a";
 
 export const r2BucketName = process.env.R2_BUCKET_NAME || "easyfisk-docs";
 export const publicDomainURL = process.env.R2_PUBLIC_DOMAIN_URL || "https://ocpp-labs.com";
